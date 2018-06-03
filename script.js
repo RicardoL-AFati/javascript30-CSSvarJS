42db4863716207413b87aaf597b383048c0b77cf
RicardoL-AFati <rickydem24@gmail.com>
2018-06-03 15:22:18 -0600

    added comments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// Counter is global scope
 let counter = 0;
+// object of images to cycle through
 const imageSources = {
   0: 'https://source.unsplash.com/iYQC9xWMvw4/400x300',
   1: 'https://source.unsplash.com/iab4hAhcNcI/400x300',
@@ -9,17 +11,22 @@ const imageSources = {
 };
 
 const image = document.querySelector('img');
-
+/* Each input has: name, value, and data-sizing property (among others)
+   name - corresponds to a CSS variable. value - value to set CSS var to 
+   data-sizing is 'suffix' for value (px) */
 function handleUpdate() {
+  // Set CSS var (--this input's name) to input's value + suffix
   document.documentElement.style.setProperty(`--${this.name}`, this.value + this.dataset.sizing);
 }
-
+// Cycles through imageSources, changes image src.
 function changeImage() {
+  // If counter is out of range - reset to beginning or end
   if (counter < 0) {
     counter = 5;
   } else if (counter > 5) {
     counter = 0;
   }
+  // assign image src to current link. Increment or decrement depending on button
   if (this.textContent === '-') {
     image.src = imageSources[counter];
     counter--;
@@ -28,10 +35,12 @@ function changeImage() {
     counter++;    
   }
 }
+
 const inputs = document.querySelectorAll('.controls input');
+/* For every input element - Call handleUpdate when there is a change to input value 
+   or mouse is moved over input */ 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-
+// Selecting buttons and adding click event to each one - Call changeImage
 const imageButtons = document.querySelectorAll('button');
-
 imageButtons.forEach(button => button.addEventListener('click', changeImage));
